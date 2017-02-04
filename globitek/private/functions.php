7626d07d86947ced5d1c2d05ed1dcf297b20fa17
Robert <rl78746@yahoo.com>
2017-02-04 13:30:44 -0600

    initial commit; added user table; test form submission detection; added has_length and is_blank to private functions; added basic form and labels to registration page

---
+++ globitek/private/functions.php
@@ -0,0 +1,56 @@
+<?php
+
+  function h($string="") {
+    return htmlspecialchars($string);
+  }
+
+  function u($string="") {
+    return urlencode($string);
+  }
+
+  function raw_u($string="") {
+    return rawurlencode($string);
+  }
+
+  function redirect_to($location) {
+    header("Location: " . $location);
+    exit;
+  }
+
+  function is_post_request() {
+    return $_SERVER['REQUEST_METHOD'] == 'POST';
+  }
+
+  function display_errors($errors=array()) {
+    $output = '';
+    if (!empty($errors)) {
+      $output .= "<div class=\"errors\">";
+      $output .= "Please fix the following errors:";
+      $output .= "<ul>";
+      foreach ($errors as $error) {
+        $output .= "<li>{$error}</li>";
+      }
+      $output .= "</ul>";
+      $output .= "</div>";
+    }
+    return $output;
+  }
+  
+  function is_blank($value='') {
+    return !isset($value) || trim($value) == '';
+  }
+  
+  function has_length($value, $options=[]) {
+    $length = strlen($value);
+    if(isset($options['max']) && ($length > $options['max'])) {
+      return false;
+    } elseif(isset($options['min']) && ($length < $options['min'])) {
+      return false;
+    } elseif(isset($options['exact']) && ($length != $options['exact'])) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+?>
---
+++ globitek/public/register.php
@@ -0,0 +1,87 @@
+<?php
+  require_once('../private/initialize.php');
+
+  // Set default values for all variables the page needs.
+
+  // if this is a POST request, process the form
+  // Hint: private/functions.php can help
+
+    // Confirm that POST values are present before accessing them.
+
+    // Perform Validations
+    // Hint: Write these in private/validation_functions.php
+
+    // if there were no errors, submit data to database
+
+      // Write SQL INSERT statement
+      // $sql = "";
+
+      // For INSERT statments, $result is just true/false
+      // $result = db_query($db, $sql);
+      // if($result) {
+      //   db_close($db);
+
+      //   TODO redirect user to success page
+
+      // } else {
+      //   // The SQL INSERT statement failed.
+      //   // Just show the error, not the form
+      //   echo db_error($db);
+      //   db_close($db);
+      //   exit;
+      // }
+
+?>
+
+<?php $page_title = 'Register'; ?>
+<?php include(SHARED_PATH . '/header.php'); ?>
+
+<div id="main-content">
+  <h1>Register</h1>
+  <p>Register to become a Globitek Partner.</p>
+
+  <?php
+    // TODO: display any form errors here
+    // Hint: private/functions.php can help
+	function isValidForm() {
+		$fields = ["first_name", "last_name", "email", "username", "password"]
+		
+		foreach($fields as $field) {
+			//check all fields are present and filled
+			if(is_blank($_POST[$field])) {
+				return false;
+			}
+			
+			//check all fields are less than 255 chars long
+			if(!has_length($_POST[$field], ['max' => 255])) {
+				return false;
+			}
+		}
+		
+		
+	}
+  ?>
+
+  <?php 
+  if(is_post_request()) {?>
+	  post
+  <?php } ?>
+  <!-- TODO: HTML form goes here -->
+  <form action="register.php" method="post">
+  <label for="firstname">First Name</label>
+  <input type="text" name="first_name" value="" id="firstname" />
+  <label for="lastname">Last Name</label>
+  <input type="text" name="last_name" value="" id="lastname"/><br />
+  <label for="email">E-mail</label>
+  <input type="text" name="email" value="" id="email"/><br />
+  <label for="username">Username</label>
+  <input type="text" name="username" value="" id="username"/><br />
+  <label for="password">Password</label>
+  <input type="password" name="password" value="" id="password"/><br />
+  <br />
+  <input type="submit" name="submit" value="Submit" />
+  </form>
+
+</div>
+
+<?php include(SHARED_PATH . '/footer.php'); ?>
