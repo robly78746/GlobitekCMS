1d4459dfd946ee24ec6eb0b0f7625202ac9859af
Robert <rl78746@yahoo.com>
2017-02-05 09:01:32 -0600

    added input sanitization (htmlspecialchars)

--- globitek/public/register.php
+++ globitek/public/register.php
@@ -3,11 +3,12 @@
 
   // Set default values for all variables the page needs.
   
-  $first_name = (isset($_POST['first_name'])) ? $_POST['first_name'] : '';
-  $last_name = (isset($_POST['last_name'])) ? $_POST['last_name'] : '';
-  $email = (isset($_POST['email'])) ? $_POST['email'] : ''; 
-  $username = (isset($_POST['username'])) ? $_POST['username'] : ''; 
-  $password = (isset($_POST['password'])) ? $_POST['password'] : '';  
+  $first_name = (isset($_POST['first_name'])) ? h($_POST['first_name']) : '';
+  $last_name = (isset($_POST['last_name'])) ? h($_POST['last_name']) : '';
+  $email = (isset($_POST['email'])) ? h($_POST['email']) : ''; 
+  $username = (isset($_POST['username'])) ? h($_POST['username']) : ''; 
+  $password = (isset($_POST['password'])) ? h($_POST['password']) : '';  
+  
   // if this is a POST request, process the form
   // Hint: private/functions.php can help
 
@@ -71,62 +72,66 @@
 		}
 		
 		if($allFieldLengthsValid === false) {
-			array_push($errors, "All fields must be no longer than 255 characters long.");
+			array_push($errors, "All fields must be no longer than 255 characters.");
 		}
 		
 		//first and last name should have at least 2 characters
 		if(!has_length($_POST["first_name"], ['min' => 2])) {
-			array_push($errors, "The first name must be at least 2 characters long.");
+			array_push($errors, "First name must be at least 2 characters.");
 		}
 		//separate just in case we need to differentiate between the two later
 		if(!has_length($_POST["last_name"], ['min' => 2])) {
-			array_push($errors, "The last name must be at least 2 characters long.");
+			array_push($errors, "Last name must be at least 2 characters.");
 		}
 		
 		//username should have at least 8 characters
 		if(!has_length($_POST["username"], ['min' => 8])) {
-			array_push($errors, "The username must be at least 8 characters long.");
+			array_push($errors, "Username must be at least 8 characters.");
 		}
 		
 		//email should contain "@"
 		if(!has_valid_email_format($_POST["email"])) {
-			array_push($errors, "The email must contain \"@\"");
+			array_push($errors, "The email must be a valid format.");
 		}
 		
 		return $errors;
 	}
-  ?>
-
-  <?php 
-  if(is_post_request()) {?>
-	  post
-  <?php
-    $errorList = isValidForm();
-	if(empty($errorList)){ 
+	
+	function submitFormDataToDB($data) {
 		$conn = db_connect();
-		$current_dateTime = date("Y-m-d H:i:s");
-		$sql_query = 'INSERT INTO users '. '(first_name, last_name, email, username, created_at) '. "VALUES ('{$first_name}', '{$last_name}', '{$email}', '{$username}', '{$current_dateTime}');";
+		$fields = '('. implode(',', array_keys($data)). ')';
+		$quotedValues = [];
+		foreach(array_values($data) as $value) {
+			array_push($quotedValues, "'".$value."'");
+		}
+		$values = " VALUES (". implode(',', $quotedValues) . ');';
+		$sql_query = 'INSERT INTO users '. $fields.$values;
 		echo $sql_query;
 		db_query($conn, $sql_query);
 		db_close($conn);
+	}
+  if(is_post_request()) {
+    $errorList = isValidForm();
+	if(empty($errorList)){ 
+		$currentDateTime = date("Y-m-d H:i:s");
+		$formFields = ['first_name' => $first_name, 'last_name' => $last_name, 'email' => $email, 'username' => $username, 'created_at' => $currentDateTime];
+		submitFormDataToDB($formFields);
 		redirect_to("registration_success.php");
-	?>
-	valid form submitted
-	<?php } else { 
+	} else { 
 		echo display_errors($errorList);
 	}
   } ?>
   <!-- TODO: HTML form goes here -->
   <form action="register.php" method="post">
-  <label for="firstname">First Name</label>
-  <input type="text" name="first_name" value="<?php echo $first_name; ?>" id="firstname" />
-  <label for="lastname">Last Name</label>
+  <label for="firstname">First Name:</label><br />
+  <input type="text" name="first_name" value="<?php echo $first_name; ?>" id="firstname" /><br />
+  <label for="lastname">Last Name:</label><br />
   <input type="text" name="last_name" value="<?php echo $last_name; ?>" id="lastname"/><br />
-  <label for="email">E-mail</label>
+  <label for="email">E-mail:</label><br />
   <input type="text" name="email" value="<?php echo $email; ?>" id="email"/><br />
-  <label for="username">Username</label>
+  <label for="username">Username:</label><br />
   <input type="text" name="username" value="<?php echo $username; ?>" id="username"/><br />
-  <label for="password">Password</label>
+  <label for="password">Password:</label><br />
   <input type="password" name="password" value="<?php echo $password; ?>" id="password"/><br />
   <br />
   <input type="submit" name="submit" value="Submit" />
